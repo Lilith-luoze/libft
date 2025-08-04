2a3262594704777012f78f920e7d8845c0557b28
Luoze Guo <luozguo@1-C-31.42heilbronn.de>
2025-08-04 18:22:51 +0200

    doing memmove

--- part_1_review_old_function.c
+++ part_1_review_old_function.c
@@ -60,8 +60,83 @@ size_t	ft_strlen(const char *s)
     return (i);
 }
 
+// memset : set a block of memory to a value.
+void *ft_memset(void *b, int c, size_t len)
+{
+	unsigned char *ptr = (unsigned char *)b;
+	unsigned char ar = c;
+	size_t i = 0;
 
+	while (i < len)
+	{
+		ptr[i] = ar;
+		i++;
+	}
 
+	return (b);
+}
+// bzero : be zero, mem!
+void	ft_bzero(void *s, size_t n)
+{
+	unsigned char *ptr = (unsigned char *)s;
+	size_t i = 0;
+
+	while (i < n)
+	{
+		ptr[i] = 0;
+		i++;
+	}
+
+	return;
+}
+
+// memcpy : undefined if dst overlap with src
+void *ft_memcpy(void * dst, const void * src, size_t n)
+{
+	size_t i = 0;
+	unsigned char *ptr_dst = (unsigned char *)dst;
+	const unsigned char *ptr_src = (const unsigned char *)src;
+
+	while(i < n)
+	{
+		ptr_dst[i] = ptr_src[i];
+		i++;
+	}
+	return (dst);
+}
+
+// memmove : Non-destructive manner in terms of the data stored in src
+// How to check the memory overlapping? 
+void *ft_memmove(void *dst, const void *src, size_t len)
+{
+	size_t i = 0;
+	unsigned char *ptr_dst = (unsigned char *)dst;
+	const unsigned char *ptr_src = (const unsigned char *)src;
+	// go through the mem block to check overlapping
+	// 1. fix dst, check src - str src is pre or overlapped with dst, or not.
+	while (i < len)
+	{
+		if (ptr_src + i == ptr_dst)
+		{
+			//call function static backward_cp
+			return (dst);
+		}
+		else
+			i++;
+	}
+	i = 0;
+	while (i < len)
+	{
+		if (ptr_src == ptr_dst + i)
+		{
+			//call function static forward_cp
+			return (dst);
+		}
+		else
+			i++;
+	}
+	return (dst);
+}
 
 
 
